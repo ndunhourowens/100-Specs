ea7ef112aa1becf9ccc02dfa83e77b8365f6b846
Naomi Owens <ndunhourowens@Naomis-MacBook-Pro.local>
2015-09-26 10:10:37 -1000

    create a literal object named 'gender'. Contains three properties.  Female: 'female', male: 'male', and unknown: 'undefined'

--- public/js/class-definitions.js
+++ public/js/class-definitions.js
@@ -73,7 +73,11 @@ var club_name = 'Fight Club';
  *   male    => "male"
  *   unknown => undefined
  */
-
+var gender = {
+  female: 'female',
+  male: 'male',
+  unknown: undefined
+};
 
 /* Step 10
  *
