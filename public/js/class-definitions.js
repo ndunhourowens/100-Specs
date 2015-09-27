f1c4b1badd3282c3331db4305aa1a605d218f651
Naomi Owens <ndunhourowens@Naomis-MacBook-Pro.local>
2015-09-26 21:23:43 -1000

    create Cookie method with one param.

--- public/js/class-definitions.js
+++ public/js/class-definitions.js
@@ -1277,7 +1277,12 @@ Lightbulb.prototype.flipSwitch = function(on) {
  * and the dayOfTheWeek is "Monday", return true.  Else return false.
  *
  */
-
+Cookie.prototype.swipedByCookieMonster = function(dayOfTheWeek) {
+  if(this.flavor === 'chocolate' && dayOfTheWeek === 'Monday') {
+    return true;
+  }
+  return false;
+};
 
  /* Step 90
  *
